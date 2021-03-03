d6e58f117e4fc134ee3a93b857291b2b3ee83f52
Robert Watt <rwatt@four51.com>
2021-03-03 12:53:20 -0600

    Completed tutorial

---
+++ components/your-component.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+const YourComponent = () => {
+    <Image
+        src="/images/profile.jpg"
+        height={144}
+        width={144}
+        alt="Robert Watt"
+    />
+}
+
+export default YourComponent;
