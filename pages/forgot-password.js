37b230650d1f35cfdafd01a7d2c580e6f8353145
Robert Watt <rwatt@four51.com>
2021-03-05 05:12:03 -0600

    Update text on forgot-password

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -7,7 +7,8 @@ export default function ForgotPassword() {
         <div className="space-y-8">
             <div>
                 <h2 className="text-2xl mb-1">Jane Doe</h2>
-                <p className="mb-3 text-gray-500">This user is not a part of the "discount" user group so you will see $10.00 price on "Cotton T-shirt". <b>This is also the default context user, so the <code>OrderCloudContext.user</code> object will look very similar.</b></p>
+                <p className="mb-3 text-gray-500">This user <b>is not a member</b> of the "discount" user group so you will see $10.00 price on "Cotton T-shirt".</p>
+                <p className="mb-3 text-gray-500">This is also the default context user, so the <code>OrderCloudContext.user</code> object will look very similar to when you are <em>unauthenticated</em>.</p>
                 <pre className="text-red-500">
                     <code>{`Username: buyer01\nPassword: password1234`}</code>
                 </pre>
@@ -15,7 +16,7 @@ export default function ForgotPassword() {
             <hr/>
             <div>
                 <h2 className="text-2xl mb-1">John Smith</h2>
-                <p className="mb-3 text-gray-500">This user is a member of the "discount" user group so you will see an $8.00 price on "Cotton T-shirt".</p>
+                <p className="mb-3 text-gray-500">This user <b>is a member</b> of the "discount" user group so you will see an $8.00 price on "Cotton T-shirt".</p>
                 <pre>
                     <code className="text-red-500">{`Username: buyer02\nPassword: password1234`}</code>
                 </pre>
