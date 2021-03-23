86d742f7f49b57d384a07d9afae7d57db1f33c8d
Robert Watt <rwatt@four51.com>
2021-03-22 21:34:54 -0500

    Add placeholders for product list/detail using material/labs skeleton

--- components/oc-product-detail.tsx
+++ components/oc-product-detail.tsx
@@ -6,6 +6,7 @@ import useOcProductDetail from "../lib/useOcProductDetail"
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { green } from "@material-ui/core/colors"
 import Divider from "@material-ui/core/Divider"
+import Skeleton from "@material-ui/lab/Skeleton"
 
 const useStyles = makeStyles((theme:Theme) => createStyles({
     name: {
@@ -50,7 +51,14 @@ export default function OcProductDetail({productId}) {
                         <Typography variant="h4" component="p" className={classes.price}>{price}</Typography>
                     </Box>
                     ) : (
-                        <h1>Product Data Loading...</h1>
+                    <Box display="flex" alignItems="center">
+                        <div className={classes.grow}>
+                            <Skeleton variant="text" animation="pulse" height={32} width={400}/>
+                            <Divider className={classes.divider}/>
+                            <Skeleton variant="text" animation="pulse" height={48} width={600}/>
+                        </div>
+                        <Skeleton variant="text" animation="pulse" height={41} width={200}/>
+                    </Box>
                 )}
             </Box>
         </Container>
--- components/oc-product-list.tsx
+++ components/oc-product-list.tsx
@@ -1,13 +1,14 @@
 import Box from '@material-ui/core/Box'
 import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
 import CardContent from '@material-ui/core/CardContent'
 import Container from "@material-ui/core/Container"
 import Grid from "@material-ui/core/Grid"
-import GridListTile from "@material-ui/core/GridListTile"
 import Typography from '@material-ui/core/Typography'
+import Skeleton from "@material-ui/lab/Skeleton"
 import useOcProductList from "../lib/useOcProductList"
 import CardActionAreaLink from './cardActionAreaLink'
+
+const placeholderArray = [1,1,1,1,1,1];
 
 export default function OcProductList({page, pageSize}) {
 
@@ -19,26 +20,26 @@ export default function OcProductList({page, pageSize}) {
     return (
         <Box marginY={4}>
             <Container maxWidth="md">
-                {items ? (
-                    <Grid container spacing={2}>
-                        {items.map((p, i) => (
+                <Grid container spacing={2}>
+                    {items ? items.map((p, i) => (
+                        <Grid item key={i} xs={12} sm={6} md={4}>
+                            <Card style={{height:'100%'}}>
+                                <CardActionAreaLink href={`/products/${p.ID}`}>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h6" component="h2">
+                                            {p.Name}
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p"  dangerouslySetInnerHTML={{ __html: p.Description}}/>
+                                    </CardContent>
+                                </CardActionAreaLink>
+                            </Card>
+                        </Grid>
+                    )) : placeholderArray.map((_, i) => (
                             <Grid item key={i} xs={12} sm={6} md={4}>
-                                <Card style={{height:'100%'}}>
-                                    <CardActionAreaLink href={`/products/${p.ID}`}>
-                                        <CardContent>
-                                            <Typography gutterBottom variant="h6" component="h2">
-                                                {p.Name}
-                                            </Typography>
-                                            <Typography variant="body2" color="textSecondary" component="p"  dangerouslySetInnerHTML={{ __html: p.Description}}/>
-                                        </CardContent>
-                                    </CardActionAreaLink>
-                                </Card>
+                                <Skeleton height={111} variant="rect" animation="wave" width="100%"/>
                             </Grid>
-                        ))}
-                    </Grid>
-                ) : (
-                    <h1>Loading Products</h1>
-                )}
+                    ))}
+                </Grid>
             </Container>
         </Box>
     )
