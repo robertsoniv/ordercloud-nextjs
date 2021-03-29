ceb716dba938dada754acf1a2de0b710e9a7608c
Robert Watt <rwatt@four51.com>
2021-03-29 10:33:28 -0500

    Rename files temporarily

--- components/OcProductList.tsx
+++ components/OcProductList.tsx
@@ -7,7 +7,7 @@ import Skeleton from "@material-ui/lab/Skeleton"
 import { Filters } from 'ordercloud-javascript-sdk'
 import { FunctionComponent, useMemo } from 'react'
 import useOcProductList from "../lib/useOcProductList"
-import CardActionAreaLink from './CardActionAreaLink'
+import CardActionAreaLink from './Card-ActionAreaLink'
 
 interface OcProductListProps {
     catalogId?: string;
